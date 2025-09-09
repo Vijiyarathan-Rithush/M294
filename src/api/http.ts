f7e591168fdd3bd0967fbbc9ebea9fb473b2b8ff
Vijiyarathan-Rithush <vijiyarathan.rithush@gmail.com>
2025-09-09 09:26:11 +0200

    init

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,6 @@
-import axios from "axios"
+import axios from "axios";
 
-export const http = axios.create
-({
+export const http = axios.create({
     baseURL: "http://localhost:8000",
     timeout: 8000,
     headers: {Accept: "Application/json"},
