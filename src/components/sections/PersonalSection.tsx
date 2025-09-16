d70882f5588d390f530532576585a7b5346af39b
Vijiyarathan-Rithush <vijiyarathan.rithush@gmail.com>
2025-09-16 11:13:33 +0200

    anpassungen

--- src/components/sections/PersonalSection.tsx
+++ src/components/sections/PersonalSection.tsx
@@ -11,7 +11,14 @@ export default function PersonalSection()
     <>
       <InputField label="Full Name" id="name" {...register("name", validationRules.name)} error={errors.name} isValid={!!dirtyFields.name && !errors.name} />
       <PhoneInputField label="Phone Number" type="tel" placeholder="12 345 67 89" {...register("phoneNumber", validationRules.phoneNumber)} error={errors.phoneNumber} isValid={!!dirtyFields.phoneNumber && !errors.phoneNumber} />
-      <InputField label="E-Mail" id="email" type="email" {...register("email", validationRules.email)} error={errors.email} isValid={!!dirtyFields.email && !errors.email} />
+      <InputField
+        label="E-Mail"
+        id="email"
+        type="email"
+        {...register("email", validationRules.email)}
+        error={errors.email}
+        isValid={!!dirtyFields.email && !errors.email}
+      />
     </>
   );
 }
